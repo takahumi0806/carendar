3bdaa73a86a67fe75fce50e97b99a8265d4122f1
takahumi0806 <yabuta0439@hotmail.com>
2022-10-22 18:43:25 +0900

    seed

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,11 @@
+
+require('dotenv').config();
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_SERVER,
+    username: process.env.MYSQL_ROOT_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_ROOT_DATABASE,
+    host: process.env.MYSQL_ROOT_SERVER,
     dialect: 'mysql',
   },
   test: {
