6ec002f5efa8a5cf9e6190dc75a01dda938e2865
takahumi0806 <yabuta0439@hotmail.com>
2022-11-15 23:50:28 +0900

    sort

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -11,6 +11,11 @@ module.exports = {
       res.redirect('/');
     }
     const messages = await Users.allMessage();
+    // messages.sort(function (a, b) {
+    //   if (a.createdAt < b.createdAt) return -1;
+    //   if (a.createdAt > b.createdAt) return 1;
+    //   return 0;
+    // });
     const user = await Users.loginUser(req.user.token);
     if (!messages) {
       messages = [{ title: '', content: '', user: { name: '' } }];
