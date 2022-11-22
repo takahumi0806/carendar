290be12f36673de4683cf766381d8677889df010
takahumi0806 <yabuta0439@hotmail.com>
2022-11-22 18:19:34 +0900

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,7 +21,7 @@ router.get('/users', userController.myPage);
 router.get('/register', userController.register);
 router.post('/signup', userRegistValidator, authController.postUser);
 router.post('/logout', authController.logout);
-router.post('/messages', postRegistValidator, PostController. newMessage);
+router.post('/messages', postRegistValidator, PostController.newMessage);
 router.get('/messages', PostController.message);
 router.get('/messages/:id', PostController.message);
 router.get('/messages/:id/delete', PostController.deleteMessage);
