03a8683e9b3461684291050db88b23b822fdb2be
takahumi0806 <yabuta0439@hotmail.com>
2022-12-16 22:18:06 +0900

    Update MessagelikeController.js

--- src/controllers/MessagelikeController.js
+++ src/controllers/MessagelikeController.js
@@ -1,23 +1,23 @@
 const models = require('../models');
 module.exports = {
-  async addLike(req, res){
+  async addLike(req, res) {
     if (!req.user) {
       res.redirect('/');
     }
     await models.MessageLikes.addlike(req.body, req.user);
     const messages = await models.Messages.allMessage();
     const user = await models.user.loginUser(req.user.token);
-    const likes = await models.Messages.countLike()
+    const likes = await models.Messages.countLike();
     res.render('mypage', { user, messages, likes });
   },
-  async deleteLike(req, res){
+  async deleteLike(req, res) {
     if (!req.user) {
       res.redirect('/');
     }
     await models.MessageLikes.deletelike(req.params.id, req.user);
     const messages = await models.Messages.allMessage();
     const user = await models.user.loginUser(req.user.token);
-    const likes = await models.Messages.countLike()
+    const likes = await models.Messages.countLike();
     res.render('mypage', { user, messages, likes });
-  }
-}
+  },
+};
