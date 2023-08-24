3c7a4b9662da24de9bc07ea228b947b2a0b2fabf
takahumi0806 <yabuta0439@hotmail.com>
2023-08-25 04:28:11 +0900

    tablecreate

---
+++ src/migrations/20230824191637-Users.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        unique: true,
+        type: Sequelize.STRING
+      },
+      mail: {
+        unique: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      passwordconfirm: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
